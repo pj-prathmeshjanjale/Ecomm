00ec9532c4d2c329748dafeabaa5a5055f6082ab
PJ <prathmeshjanjalepj@gmail.com>
2025-07-19 16:50:39 +0530

    third commit

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
-const connectDB = () => {
-    try {
-       mongoose.connect(process.env.MONGO_URI).then(()=>{
-        console.log(("mongodb connected"));
-       })
-    } catch (error) {
-        console.log(error);
-    }
-}
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true, //  Ensures encrypted connection
+      retryWrites: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error(" MongoDB connection error:", error.message);
+    process.exit(1); // Exit the app if DB connection fails
+  }
+};
 
 module.exports = connectDB;
