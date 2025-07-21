7e323568795893d37fb17d249e2e1e6915261e2e
PJ <prathmeshjanjalepj@gmail.com>
2025-07-21 14:34:55 +0530

    updated cors

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 app.use(cors());
+
+app.use(cors({
+  origin: [
+    "http://localhost:3000",   
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}));
+
 const connectDB = require("./config/mongodb");
 const indexRoutes = require("./routes/index.routes")
 const userRoutes = require("./routes/user.routes")
